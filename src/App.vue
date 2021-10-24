489bffad1f1ce0440aaf85b7ea30d63a27b6f1c5
Hamada Fadil Mahdi <hamadamahdi2014@gmail.com>
2021-10-24 21:20:53 +0100

    made structure of search field

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,45 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+    <v-row class="red-b">
+      <v-col
+        cols="12"
+        class="px-12 mt-12"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+        <v-text-field
+          v-model="city"
+          label="Search for your city..."
+          solo
+          clearable
+          prepend-inner-icon="mdi-domain"
+          append-outer-icon="mdi-map-marker"
+        ></v-text-field>
+      </v-col>
+    </v-row>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
-    //
+    city: ""
   }),
 };
 </script>
+
+<style>
+  .red-b {
+    border: 2px red solid;
+  }
+  .green-b {
+    border: 2px green solid;
+  }
+</style>
+
